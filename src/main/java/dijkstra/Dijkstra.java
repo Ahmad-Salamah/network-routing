e6dcd4607320a1dab92701282c67602492e10f69
ahmad <ahmad.h.salamah.1999@gmail.com>
2022-06-11 19:18:34 +0300

    Adds Dijkstra class

---
+++ src/main/java/dijkstra/Dijkstra.java
@@ -0,0 +1,62 @@
+package dijkstra;
+
+import graph.Edge;
+import graph.Graph;
+import graph.Vertex;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class Dijkstra {
+    public static void compute(Vertex source, Vertex target) {
+        PriorityQueue<Vertex> queue = new PriorityQueue<>();
+        source.setVisited(true);
+        source.setCost(0);
+        for(int i = 0 ; i < source.getAdj().size() ; i++){
+            Vertex adj = source.getAdj().get(i).getDestination();
+            double cost = source.getAdj().get(i).getDistance();
+            adj.setCost(cost);
+            adj.setSource(source);
+            queue.add(adj);
+        }
+        while (!queue.isEmpty() && !target.isVisited()){
+            Vertex vertex = queue.poll();
+            vertex.setVisited(true);
+            for(int i = 0 ; i < vertex.getAdj().size() ; i++){
+                Vertex adj = vertex.getAdj().get(i).getDestination();
+                if(!adj.isVisited()){
+                    double cost = vertex.getAdj().get(i).getDistance() + vertex.getCost();
+                    if(cost < adj.getCost()){
+                        adj.setCost(cost);
+                        adj.setSource(vertex);
+                        if(queue.contains(adj)){
+                            queue.remove(adj);
+                        }
+                        queue.add(adj);
+                    }
+                }
+            }
+        }
+
+    }
+
+    public static List<Vertex> getPath(Vertex source , Vertex target ){
+        if(target.getSource() == null) {
+            return null;
+        }
+        List<Vertex> path =  new ArrayList<>();
+        path.add(target);
+        Vertex vertex = target;
+        while (vertex.getSource() != null){
+            vertex = vertex.getSource();
+            path.add(vertex);
+        }
+        List<Vertex> path2 =  new ArrayList<>();
+        for(int i = path.size()-1 ; i >=0 ; i --) {
+            path2.add(path.get(i));
+        }
+        return path2;
+    }
+
+}
