c7305729c1c5d36b51b9ed1c0baf7b002e5804ed
ahmad <ahmad.h.salamah.1999@gmail.com>
2022-06-11 19:14:03 +0300

    Changes main Scene Size to 1000*567

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
-        Scene scene = new Scene(root, 898, 567);
+        Scene scene = new Scene(root, 1000, 567);
 
         stage.setTitle("Networking_Routing");
         stage.setScene(scene);
